ef5fbe191fb4f50df4d4816657daab193c819eb0
FrancescoZeno <zenofrancesco2@gmail.com>
2026-07-03 14:07:00 +0200

    1.0

---
+++ CrackTheCaseIos/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CrackTheCaseIos
+//
+//  Created by AFP PAR 049 on 01/07/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
